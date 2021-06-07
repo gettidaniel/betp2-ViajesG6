3d202e764d14065fb76db6f664d18445ca0aad1f
santiago fernandez <sanfernz_88@hotmail.com>
2021-06-06 22:24:55 -0300

    usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+const dataUser = require('../data/userdb');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET usuarios listing. */
+router.get('/', async (req, res) => {
+    const usuarios = await dataUser.getUsuarios();
+    res.json(usuarios);
 });
 
-module.exports = router;
+router.get('/:id', async (req, res) => {
+    const usuario = await dataUser.getUsuario(req.params.id);
+    res.json(usuario);
+    /* if (usuario == null) {
+        res.status(404);
+    } else {
+        res.json(usuario);
+    } */
+    //si no lo encuentra que mande un 404 recurso no encontrado. Al ser Json devuelve vacio.
+});
+
+router.post('/', async (req, res) => {
+    //TODO Validacion
+    let usuario = req.body;
+    usuario = await dataUser.addUsuario(usuario);
+    res.json(usuario);
+});
+
+
+router.put('/:id', async (req, res) => {
+    //TODO Validacion
+    let id = req.params.id;
+    let usuario = req.body;
+    usuario._id = id;
+    usuario = await dataUser.updateUsuario(usuario);
+    res.json(usuario);
+});
+
+router.delete('/:id', async (req, res) => {
+    let id = req.params.id;
+    await dataUser.deleteUsuario(id)
+    res.send('Eliminado');
+});
+
+module.exports = router
--- routes/vuelos.js
+++ routes/vuelos.js
@@ -1,33 +1,45 @@
 var express = require('express');
 var router = express.Router();
-const dataVuelo = require('../data/vuelo')
+const dataVuelo = require('../data/vuelodb');
 
 /* GET vuelos listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', async (req, res) => {
     const vuelos = await dataVuelo.getVuelos();
     res.json(vuelos);
 });
 
-router.get('/:id' , async (req, res) => {
+router.get('/:id', async (req, res) => {
     const vuelo = await dataVuelo.getVuelo(req.params.id);
     res.json(vuelo);
-})
+    /* if (vuelo == null) {
+        res.status(404);
+    } else {
+        res.json(vuelo);
+    } */
+    //si no lo encuentra que mande un 404 recurso no encontrado. Al ser Json devuelve vacio.
+});
 
-router.post('/', async (req,res)=>{
+router.post('/', async (req, res) => {
     //TODO Validacion
     let vuelo = req.body;
     vuelo = await dataVuelo.addVuelo(vuelo);
     res.json(vuelo);
-})
+});
 
 
-router.put('/:id', async (req,res)=>{
+router.put('/:id', async (req, res) => {
     //TODO Validacion
     let id = req.params.id;
     let vuelo = req.body;
-    vuelo.id = id;
+    vuelo._id = id;
     vuelo = await dataVuelo.updateVuelo(vuelo);
     res.json(vuelo);
-})
+});
+
+router.delete('/:id', async (req, res) => {
+    let id = req.params.id;
+    await dataVuelo.deleteVuelo(id)
+    res.send('Eliminado');
+});
 
 module.exports = router;
